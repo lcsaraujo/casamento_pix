87649225b6a43fef3aa8e5d15b59d4141c27b1ef
Lucas Araujo <44172124+lcsaraujo@users.noreply.github.com>
2024-07-29 22:41:54 -0300

    Corrigido erro que não aparecia o QRCode

--- app/credentials.php
+++ app/credentials.php
@@ -2,23 +2,15 @@
 
     // https://www.mercadopago.com.br/developers/pt/docs/credentials
     const MERCADO_PAGO_CONFIG = [
-        "access_token"     => "APP_USR-6329549945459484-072919-1acc7364aa065f19e76dfdc924b00609-1314974383",
-        // "access_token"     => "TEST-635489786264208-072512-b34692c4ff1ff831bac57ece8199ac85-1314974383",
-        "notification_url" => "https://casamentopix.x10.bz/public/payment/notification.php"
+        "access_token"     => "Insira aqui o Access_Token",
+        // "access_token"     => "Insira aqui o Access_Token",
+        "notification_url" => "https://meu.site/public/payment/notification.php"// Retirar o "meu.site" e colocar seu site.
     ];
-
-    // const DATABASE_CONFIG = [
-    //     "drive"  => "mysql",
-    //     "host"   => "localhost",
-    //     "user"   => "root",
-    //     "pass"   => "root",
-    //     "dbname" => "pix"
-    // ];
 
     const DATABASE_CONFIG = [
         "drive"  => "mysql",
         "host"   => "localhost",
-        "user"   => "akzbohgt_pix",
-        "pass"   => "68aA8F2ehttAgrS3MQzH",
-        "dbname" => "akzbohgt_pix"
+        "user"   => "root",
+        "pass"   => "root",
+        "dbname" => "pix"
     ];
